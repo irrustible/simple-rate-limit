3cade587c84b0f4d0ab111b378fb09c777d65897
James Laver <james.laver@gmail.com>
2020-08-11 11:45:10 +0200

    v0.1.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# simple-rate-limit
+
+[![License](https://img.shields.io/crates/l/simple-rate-limit.svg)](https://github.com/irrustible/simple-rate-limit/blob/main/LICENSE)
+[![Package](https://img.shields.io/crates/v/simple-rate-limit.svg)](https://crates.io/crates/simple-rate-limit)
+[![Documentation](https://docs.rs/simple-rate-limit/badge.svg)](https://docs.rs/simple-rate-limit)
+
+TODO: Usage.
+
+## Status
+
+Brand new, but seems to work. Lacks automated tests, but has manual ones.
+
+## Copyright and License
+
+    Copyright (c) 2020 James Laver, simple-rate-limit contributors.
+    
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use ring_vec::RingVec;
 use std::time::{Duration, Instant};
 
+/// A [`RateLimit`] is how many times an event may occur in a given period of time.
 #[derive(Clone, Copy, Eq, PartialEq)]
 pub struct RateLimit {
     pub count: usize,
@@ -8,6 +9,8 @@ pub struct RateLimit {
 }
 
 impl RateLimit {
+    /// Creates a new [`RateLimit`] with the provided values.
+    /// Warning: Panics if count is zero.
     pub fn new(count: usize, period: Duration) -> Option<RateLimit> {
         if count > 0 {
             Some(RateLimit { count, period })
@@ -17,38 +20,45 @@ impl RateLimit {
     }
 }
 
+/// A [`RateLimiter`] enforces a [`RateLimit`] by maintaining a ring
+/// vector of timestamps capped at the [`RateLimit::count`].
 pub struct RateLimiter {
     pub limit: RateLimit,
     readings: RingVec<Instant>,
 }
 
 impl RateLimiter {
-    // If you expect to use this lightly
+    /// Creates a new `RateLimiter` without preallocating
+    /// storage. Ideal if you might never use it.
     pub fn new(limit: RateLimit) -> RateLimiter {
         RateLimiter { limit, readings: RingVec::new(limit.count) }
     }
 
-    // If you expect to use this heavily
+    /// Creates a new `RateLimiter` with preallocated storage. Ideal
+    /// if you're likely to use it a lot to avoid resizing during fill.
     pub fn new_preallocated(limit: RateLimit) -> RateLimiter {
         RateLimiter { limit, readings: RingVec::new_preallocated(limit.count) }
     }
 
-    /// Check if you are still permitted to perform the action
+    /// Logs the current time with the [`RateLimiter`] and checks if
+    /// the event falls within the rate limit.
     pub fn check(&mut self) -> bool { self.check_at(Instant::now()) }
 
-    // Promise that you'll only march forwards in time and we promise
-    // to return the correct answers.
+    /// Like `check`, but you can provide an arbitrary timestamp
+    /// (useful for tests!).  Promise that you'll only march forwards
+    /// in time and we promise to return the correct answers.
     pub fn check_at(&mut self, instant: Instant) -> bool {
         if self.readings.push(instant).is_ok() { return true; }
-        let reclaimed = self.delete_outdated(instant);
+        let reclaimed = self.sweep(instant);
         if reclaimed {
             self.readings.push(instant).unwrap();
         }
         reclaimed
     }
 
-    /// Removes all the readings from longer than period ago.
-    pub fn delete_outdated(&mut self, instant: Instant) -> bool {
+    /// Removes all the readings from longer than the contained
+    /// [`RateLimit`]'s period ago, relative to the provided instant.
+    pub fn sweep(&mut self, instant: Instant) -> bool {
         let bench = instant - self.limit.period;
         let mut reclaimed = false;
         while let Some(x) = self.readings.peek() {
